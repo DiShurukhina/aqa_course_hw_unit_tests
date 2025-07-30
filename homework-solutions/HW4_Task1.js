b75c6172d10685e4756f542be0d8716ace40067b
DiShurukhina <dolbunok@gmail.com>
2025-07-30 19:21:12 +0300

    add solutions for hw_4

---
+++ homework-solutions/HW4_Task1.js
@@ -0,0 +1,13 @@
+"use strict";
+/*
+ - Создайте переменную salary со значением 1000
+  - Создайте переменную grade, которая должна получить значение "middle" если salary больше или равна 1000,
+   и значение "junior" - если меньше
+*/
+
+let salary = 100;
+let grade = (salary >= 1000) ? 'middle' : 'junior';
+console.log(grade);
+
+
+
---
+++ homework-solutions/HW4_Task3.js
@@ -0,0 +1,23 @@
+"use strict";
+/*
+Преобразовать Task 2 таким образом, чтобы значение НАПРИМЕР '2' (т.е. ЛЮБАЯ строка в которой лежат ТОЛЬКО ЦИФРЫ) пропускалось, 
+  преобразовываясь в number
+*/
+
+let minAge = 18;
+let maxAge = 60;
+
+let ageForTests = [10, 17, 18, 19, 59, 60, 61, 'test123', '33']; //добавила пару тестовых значений
+for (let age of ageForTests) {
+  if (typeof age !== 'number' && isNaN((age = parseInt(age)))) {
+    console.log('Incorrect data type');
+  } else if (age < minAge) {
+    console.log(`You don't have access cause your age is ${age}. It's less then ${minAge} `);
+  } else if (age >= minAge && age < maxAge) {
+    console.log('Welcome  !');
+  } else if (age > maxAge) {
+    console.log('Keep calm and look Culture channel');
+  } else {
+    console.log('Technical work');
+  }
+}
